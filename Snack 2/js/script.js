d8eefda82798b1fbc79487be80e3427017f1d294
ItsKristian01 <kristianhana11@gmail.com>
2023-11-08 16:59:37 +0100

    added random numbers

--- Snack 2/js/script.js
+++ Snack 2/js/script.js
@@ -1,17 +1,28 @@
 const footballTeams = [
-    {
-      nome: "Milan",
-      falliSubiti: 0,
-      puntiFatti: 0,
-    },
-    {
-      nome: "Roma",
-      falliSubiti: 0,
-      puntiFatti: 0,
-    },
-    {
-      nome: "Inter",
-      falliSubiti: 0,
-      puntiFatti: 0,
-    },
+  {
+    nome: "Milan",
+    falliSubiti: 0,
+    puntiFatti: 0,
+  },
+  {
+    nome: "Roma",
+    falliSubiti: 0,
+    puntiFatti: 0,
+  },
+  {
+    nome: "Inter",
+    falliSubiti: 0,
+    puntiFatti: 0,
+  },
 ];
+
+console.log(footballTeams);
+
+for (let i = 0; i < footballTeams.length; i++) {
+  const curTeam = footballTeams[i];
+  curTeam.falliSubiti = Math.floor(Math.random() * 100);
+  curTeam.puntiFatti = Math.floor(Math.random() * 100);
+  
+  console.log(curTeam.falliSubiti, curTeam.puntiFatti);
+}
+
