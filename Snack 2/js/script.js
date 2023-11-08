85427eace1ac5ed996db6ee5e21455b892049978
ItsKristian01 <kristianhana11@gmail.com>
2023-11-08 16:47:38 +0100

    add: starting point snack 2

---
+++ Snack 2/js/script.js
@@ -0,0 +1,17 @@
+const footballTeams = [
+    {
+      nome: "Milan",
+      falliSubiti: 0,
+      puntiFatti: 0,
+    },
+    {
+      nome: "Roma",
+      falliSubiti: 0,
+      puntiFatti: 0,
+    },
+    {
+      nome: "Inter",
+      falliSubiti: 0,
+      puntiFatti: 0,
+    },
+];
